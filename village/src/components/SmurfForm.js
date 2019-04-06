daab1e69c09df381ac73008498edf3b474d1a789
focused220 <roblhjr.220@gmail.com>
2019-04-05 19:12:49 -0700

    updated axios post

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: '',
       name: '',
       age: '',
       height: ''
     };
+    console.log(props)
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    let newSmurf = {
+      id: this.props.id,
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height,
+    }
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    Axios.post('http://localhost:3333/smurfs', newSmurf).then((res) => {
+      console.log(res.data)
+      return(
+        this.setState({
+          name: '',
+          age: '',
+          height: '',
+        }        
+        )
+      )
+    }).catch((err) => console.log(err))
+    this.props.update(newSmurf)
+    
   }
 
   handleInputChange = e => {
